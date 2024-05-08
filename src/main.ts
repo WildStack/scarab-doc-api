eb5a26ae949f81bc14ca290f1a0e0ee88770d9bf
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2024-05-08 22:09:13 +0400

    final ???

--- src/main.ts
+++ src/main.ts
@@ -9,22 +9,17 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { cyanLog } from './common/helper';
 
 NestFactory.create<NestExpressApplication>(AppModule).then(async app => {
+  console.log('='.repeat(20));
+  console.log(environment);
+
   app.enableShutdownHooks();
+  app.enableCors();
+  app.setGlobalPrefix('/api');
   app.set('trust proxy', true);
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
   app.use(compression());
-
-  // globals
-  app.enableCors();
-  app.setGlobalPrefix('/api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
 
   await app.listen(environment.PORT);
 
